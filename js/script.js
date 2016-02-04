1dd9e108ec9e76a30b908c007fb2ed3ee1875bcd
Jesper Frimann <jfrimann@gmail.com>
2016-02-04 15:03:09 +0100

    Basic functionality optimized

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 var main = function() {
 	$('a').click(function() {
-		$('a').removeClass('selected');
-		$(this).addClass('selected');
-		console.log('selection and deselction done');
-		if($(this).parent().children().length > 1) {
+		$('a').removeClass('child-selected');
+		$('a').removeClass('parent-selected');
+		
+		if($(this).parent().parent().hasClass('funky-accordion-submenu') === true){
+			$(this).addClass('child-selected');
+		}else {
+			$(this).addClass('parent-selected');
+		}
+		
+		var parentTab = $(this).parent().parent();
+		if($(this).parent().children().length > 1 | parentTab.hasClass('funky-accordion-submenu')) {
 			$(this).parent().children('.funky-accordion-submenu').toggleClass('submenu-show');
 			//alert('hello again');
-		}else{
+		}else {
 			$('.funky-accordion-submenu').removeClass('submenu-show');
+		}
+	});
+	
+	$('a').hover(function() {
+		if($(this).parent().parent().hasClass('funky-accordion-submenu') === true){
+			$(this).toggleClass('child-hover');
+		}else {
+			$(this).toggleClass('parent-hover');
 		}
 	});
 }
