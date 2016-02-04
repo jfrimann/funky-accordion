10b43c7017b708be885f4663f1af26d2459bea86
Jesper Frimann <jfrimann@gmail.com>
2016-02-04 21:22:56 +0100

    Adds '+' to menu items that have submenu items. This resolves #3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var main = function() {
+	setupAccordion();
 	$('a').click(function() {
 		$('a').removeClass('child-selected');
 		$('a').removeClass('parent-selected');
@@ -25,6 +26,29 @@ var main = function() {
 			$(this).toggleClass('parent-hover');
 		}
 	});
+}
+
+var setupAccordion = function() {
+		
+		//adds "+" to all parent level menu items that have has submenu items
+		var listItem = $('#funky-accordion-menu>ul>li');
+		listItem.each(function(li) {
+			if(listItem[li].children.length > 1) {
+				var currentLI = $(listItem[li]);
+				var newLI = $(currentLI.children('a'));
+				newLI.append('<span class="submenu-indicator">+</span>');			
+			}
+		});
+		
+		//adds "+" to all first level submenu items that have second level submenu items
+		var subListItems = $('#funky-accordion-menu>ul>li ul>li');
+		
+		subListItems.each(function(li) {
+			if(subListItems[li].children.length > 1){
+				var currentSubLI = $(subListItems[li]).children('a');
+				currentSubLI.append('<span class="submenu-indicator" style="padding-right: 10px">+</span>');
+			}
+		});
 }
 
 $(document).ready(main);
